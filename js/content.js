9c1aae263b331c1dd658bebfb6c3979f88607794
sasama <mametaku3@yahoo.co.jp>
2022-01-09 16:25:44 +0900

    Update: 動的に追加した要素に対してもイベント発火するよう修正

--- js/content.js
+++ js/content.js
@@ -1,89 +1,94 @@
-const liAttributes = [
-	'class',
-	'role'
-];
+const liAttributes = {
+	'class': 'nav-item',
+	'role': 'presentation'
+};
 
-const liValues = [
-	'nav-item',
-	'presentation'
-];
+let buttonAttributes = {
+	'class': 'nav-link', 
+	'id': 'memo', //後ほどメモ番号を追加する
+	'data-bs-toggle': 'tab',
+	'data-bs-target': '#memo', //後ほどメモ番号を追加する
+	'type': 'button',
+	'role': 'tab',
+	'aria-controls': 'memo', //後ほどメモ番号を追加する
+	'aria-selected': 'false'
+};
 
-const buttonAttributes = [
-	'class',
-	'id',
-	'data-bs-toggle',
-	'data-bs-target',
-	'type',
-	'role',
-	'aria-controls',
-	'aria-selected'
-];
-
-let buttonValues = [];
+let objUl = document.getElementById('myTab');
 
 let tabTotalCount = 3;
 
-//tabのHTML
-let createTab = (object, attributes, values) => {
-	attributes.forEach((attribute, index) => {
-		object.setAttribute(attribute, values[index]);
+//tabの属性値セット処理
+let createTab = (object, attributes) => {
+	//Object.keysで連想配列のkeyのみの配列を作成し、それに対してforEachを行なっている
+	Object.keys(attributes).forEach(key => {
+		object.setAttribute(key, attributes[key]);
 	});
+
 	return object;
 }
 
 $(function() {
-	let objUl = document.getElementById('myTab');
-
 	for (let i = 0; i < tabTotalCount; i++) {
 		let objLi = document.createElement('li');
 		let objLiCount = i + 1;
 		let objButton = document.createElement('button');
 
-		buttonValues = [
-			'nav-link', 
-			'memo' + objLiCount + '-tab',
-			'tab',
-			'#memo' + objLiCount,
-			'button',
-			'tab',
-			'memo' + objLiCount,
-			'false'
-		];
-
+		//最初のタブをactive状態にする
 		if (i == 0) {
-			buttonValues[0] = 'nav-link active';
+			buttonAttributes['class'] = 'nav-link active';
+		} else {
+			buttonAttributes['class'] = 'nav-link';
 		}
+		//tabの各属性値にメモ番号を付与する
+		buttonAttributes['id'] = 'memo' + objLiCount;
+		buttonAttributes['data-bs-target'] = '#memo' + objLiCount;
+		buttonAttributes['aria-controls'] = 'memo' + objLiCount;
 
-		objLi = createTab(objLi, liAttributes, liValues);
-		objButton = createTab(objButton, buttonAttributes, buttonValues);
+		objLi = createTab(objLi, liAttributes);
+		objButton = createTab(objButton, buttonAttributes);
 		objButton.innerHTML = 'memo' + objLiCount;
 		
 		objUl.appendChild(objLi);
 		objLi.appendChild(objButton);
 	}
 
+	//タブ追加ボタン（仮）
 	$('#tabButton').on('click', function() {
 		let objLi = document.createElement('li');
 		let objLiCount = objUl.childElementCount + 1;
 		let objButton = document.createElement('button');
 		
-		buttonValues = [
-			'nav-link', 
-			'memo' + objLiCount + '-tab',
-			'tab',
-			'#memo' + objLiCount,
-			'button',
-			'tab',
-			'memo' + objLiCount,
-			'false'
-		];
+		buttonAttributes['id'] = 'memo' + objLiCount;
+		buttonAttributes['data-bs-target'] = '#memo' + objLiCount;
+		buttonAttributes['aria-controls'] = 'memo' + objLiCount;
 
-		objLi = createTab(objLi, liAttributes, liValues);
-		objButton = createTab(objButton, buttonAttributes, buttonValues);
+
+		objLi = createTab(objLi, liAttributes);
+		objButton = createTab(objButton, buttonAttributes);
 		
 		objButton.innerHTML = 'memo' + objLiCount;
 		
 		objUl.appendChild(objLi);
 		objLi.appendChild(objButton);
+	});
+
+	//動的に追加した要素に対してもclickイベントを発火させる
+	$('#myTab').on('click', '.nav-link', function() {
+		//どのtabかの情報を取得する
+		let tabId = $(this).attr('id');
+
+		chrome.storage.local.get(tabId, function(result) {
+			$('#memoArea').val(result.tabId); //result.tabIdはない場合undefinedになる
+
+			//textaraのカウント数を反映
+			// reflectCount();
+		});
+	});
+
+	//keyup処理 keyupの都度保存するようにする？
+	$('#memoArea').keyup(function() {
+		let tabId = $('.active').attr('id');
+		chrome.storage.local.set({tabId: $('#memoArea').val()}, function(){});
 	});
 });
