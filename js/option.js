e3db4900a3199f2d7d41887c2d96624de82e2870
sasama <mametaku3@yahoo.co.jp>
2022-01-28 20:25:42 +0900

    Update: 初期化ボタンと処理を追加

--- js/option.js
+++ js/option.js
@@ -1,4 +1,6 @@
 $(function() {
+	const initialWidth = 400;
+
 	//設定画面起動時に、storageの値をスライダーに反映
 	chrome.storage.local.get('sizeOption', function(result) {
 		if (result['sizeOption'] !== undefined) {
@@ -10,6 +12,19 @@ $(function() {
 	//on('input', func)でスライダー操作時もイベント発火
 	$('#size_input').on('input', function() {
 		$('#size_value').html($(this).val());
+	});
+
+	$('#option_reset').on('click', function() {
+		let deleteConfirm = window.confirm('設定を初期化しますか？');
+
+		if (deleteConfirm) {
+			//localstorageの初期化
+			chrome.storage.local.clear();
+
+			//設定画面の入力欄部分の初期化
+			$('#size_input').val(initialWidth);
+			$('#size_value').html(initialWidth);
+		}
 	});
 
 	$('#option_save').on('click', function() {
