f5a599831717f8904995d98c05d14b3815e41d95
sasama <mametaku3@yahoo.co.jp>
2022-02-06 16:23:11 +0900

    Update: ダークモード設定処理、定数の追加

--- js/option.js
+++ js/option.js
@@ -4,22 +4,63 @@ const initialWidth = 400;
 let language = 'english';
 const LANGUAGE = {
 	'japanese': {
-		'page_title': '設定画面',
+		//id名: 表示テキスト
+		'page_title': '設定',
 		'lang_title': '言語',
 		'ja_text': '日本語',
 		'en_text': '英語',
+		'mode_title': 'ダークモード',
 		'home_text': 'ホーム',
 		'save_text': '保存',
 		'reset_text': '初期化'
 	},
 	'english': {
-		'page_title': 'OptionPage',
+		//id名: 表示テキスト
+		'page_title': 'Option',
 		'lang_title': 'Language',
 		'ja_text': 'Japanese',
 		'en_text': 'English',
+		'mode_title': 'DarkMode',
 		'home_text': 'home',
 		'save_text': 'save',
 		'reset_text': 'reset'
+	}
+}
+
+//デフォルトモード
+let mode = 'normal';
+
+//テーマカラー：ノーマルモード
+const NORMAL_MODE = {
+	'body': {
+		'background-color': '#fff',
+		'color': '#212529'
+	},
+	'.option-wrap': {
+		'background-color': '#f2f4f6'
+	}
+}
+
+//テーマカラー：ダークモード
+const DARK_MODE = {
+	'body': {
+		'background-color': '#23282F',
+		'color': '#fff'
+	},
+	'.option-wrap': {
+		'background-color': '#6b778d'
+	}
+}
+
+//テーマカラー表示用テキスト
+const MODE_TEXT = {
+	'japanese': {
+		'normal': 'デフォルト',
+		'dark': 'ダーク'
+	},
+	'english': {
+		'normal': 'Normal',
+		'dark': 'Dark'
 	}
 }
 
@@ -37,7 +78,7 @@ const OTHER_MESSAGE = {
 
 $(function() {
 	//設定画面起動時に、storageの値をスライダーに反映
-	chrome.storage.local.get(['widthOption', 'langOption'], function(result) {
+	chrome.storage.local.get(['widthOption', 'langOption', 'mode'], function(result) {
 		language = (result['langOption'] === undefined) ? 'english' : result['langOption'];
 		reflectLangOption(LANGUAGE[language]);
 
@@ -47,6 +88,14 @@ $(function() {
 			//英語がデフォルト
 			$('#lang_en').prop('checked', true);
 		}
+
+		mode = (result['mode'] === undefined) ? 'normal' : result['mode'];
+
+		if (mode === 'dark') {
+			reflectMode(DARK_MODE);
+			$('input[name="mode"]').prop('checked', true);
+		}
+		$('#mode_text').text(MODE_TEXT[language][mode]);
 	});
 
 	//ラジオボタンによる言語設定反映
@@ -61,6 +110,21 @@ $(function() {
 		}
 		//言語設定反映
 		reflectLangOption(LANGUAGE[language]);
+		$('#mode_text').text(MODE_TEXT[language][mode]);
+	});
+
+	//カラーモード変更
+	$('input[name="mode"]').click(function() {
+		let setObj = {};
+		if ($('input[name="mode"]').prop('checked')) {
+			reflectMode(DARK_MODE);
+			mode = 'dark';
+		} else {
+			reflectMode(NORMAL_MODE);
+			mode = 'normal';
+		}
+		$('#mode_text').text(MODE_TEXT[language][mode]);
+		chrome.storage.local.set(setObj, function(){});
 	});
 
 	//初期化処理
@@ -93,6 +157,13 @@ $(function() {
 			}
 		}
 
+		//カラーモード値のセット
+		if ($('input[name="mode"]').prop('checked')) {
+			setObj['mode'] = 'dark';
+		} else {
+			setObj['mode'] = 'normal';
+		}
+
 		//設定値をlocalStorageに保存
 		setObj['widthOption'] = $('#size_input').val();
 		setObj['langOption'] = language;
@@ -109,9 +180,18 @@ $(function() {
 	});
 
 	//言語設定反映
-	function reflectLangOption(Option) {
+	function reflectLangOption(Option, Mode) {
 		for (key in Option) {
 			$(`#${key}`).text(Option[key]);
+		}
+	}
+
+	//テーマカラー反映処理
+	function reflectMode(Mode) {
+		for (element in Mode) {
+			for (property in Mode[element]) {
+				$(element).css(property, Mode[element][property]);
+			}
 		}
 	}
 });
